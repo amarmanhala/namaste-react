6edc5ddba0751a71d576241d2da8af5a9d38706c
amarmanhala <amar.manhala@gmail.com>
2023-04-15 20:08:59 -0400

    Adding design

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,25 +5,26 @@ const Logo = () => {
   return (
     <div>
       <div className="logo"></div>
-    </div>
+</div>
+   
   );
 };
 
 export default function Header() {
   return (
-    <header className="header">
+    <header className="w-full h-[100px] px-14 flex flex-row bg-orange-500 justify-between items-center">
       <Logo />
       <div className="nav-items">
-        <ul>
+        <ul className="flex flex-row">
+        <Link to="/card"><li>Cart</li></Link>
           <Link to="/"><li>Home</li></Link>
           <Link to="/about"><li>About us</li></Link>
           <Link to="/contact"><li>Contact us</li></Link>
           <Link to="/instamart"><li>Instamart</li></Link>
-          <li>Cart</li>
+          <Link to="/login"><li>Login</li></Link>
+          <Link to="/signup"><li>Sign Up</li></Link>
         </ul>
       </div>
-      <button>Login</button>
-      <button>Sign in</button>
     </header>
   );
 }
