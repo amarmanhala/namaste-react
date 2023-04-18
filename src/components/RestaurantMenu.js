398a6e4b486c2dd719f33b38a419b93766b6980e
amarmanhala <amar.manhala@gmail.com>
2023-04-18 19:00:26 -0400

    Fix

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,21 +2,42 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { IMAGE_CDN_URL } from "../config";
 import useRestaurantMenu from "../../utils/useRestaurantMenu";
+import { StarRating } from "baseui/rating";
+import { HintDot, COLOR } from "baseui/badge";
+import {
+  DisplayLarge,
+  DisplayMedium,
+  DisplaySmall,
+  DisplayXSmall,
+} from 'baseui/typography';
+import {
+  ParagraphLarge,
+  ParagraphMedium,
+  ParagraphSmall,
+  ParagraphXSmall,
+  LabelLarge,
+  LabelMedium,
+  LabelSmall,
+  LabelXSmall,
+} from 'baseui/typography';
 
 const RestaurantMenu = () => {
-  //const [restaurantDetail, setRestaurantDetail] = useState({});
 
   // How to read dynamic params.
   const { id } = useParams();
 
   const restaurantDetail = useRestaurantMenu(id);
 
-  console.log(restaurantDetail);
+  console.log(restaurantDetail?.data?.cards[0].card?.card?.info);
 
   return (
-    <div>
-      <h1>Restaurant id: {id}</h1>
-      <h2>{restaurantDetail?.data?.cards[0].card?.card?.info?.name}</h2>
+    <div className="container-restaurant-menu">
+      <div>
+      <DisplayXSmall>{restaurantDetail?.data?.cards[0].card?.card?.info?.name}</DisplayXSmall>
+      <ParagraphLarge>
+        {restaurantDetail?.data?.cards[0].card?.card?.info?.avgRatingString} ratings - {restaurantDetail?.data?.cards[0].card?.card?.info?.availability?.opened === true ? (<HintDot color={COLOR.positive}>Open</HintDot>) : (<HintDot color={COLOR.negative}>Closed</HintDot>)}</ParagraphLarge>
+      </div>
+     
       <img
         src={
           IMAGE_CDN_URL +
