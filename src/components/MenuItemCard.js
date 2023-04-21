cbde3f92d76378886ce9d9b1cbca6329697d03a4
amarmanhala <amar.manhala@gmail.com>
2023-04-20 20:35:50 -0400

    Add currency

--- src/components/MenuItemCard.js
+++ src/components/MenuItemCard.js
@@ -2,12 +2,13 @@ import React from "react";
 import { MessageCard, IMAGE_LAYOUT } from "baseui/message-card";
 import { StyledDivider, SIZE } from "baseui/divider";
 import { ParagraphMedium, ParagraphSmall } from "baseui/typography";
+import { IMAGE_CDN_URL } from "../config";
 
 const CardContent = (props) => {
-  return (<><ParagraphSmall style={{ margin: 0 }}>{props.description}</ParagraphSmall><br></br><ParagraphMedium style={{ margin: 0 }}>{props.price}</ParagraphMedium></>)
+  return (<><ParagraphSmall style={{ margin: 0 }}>{props.description}</ParagraphSmall><br></br><ParagraphMedium style={{ margin: 0 }}><i>&#8377; &nbsp;</i>{Number(props.price)/100}</ParagraphMedium></>)
 }
 
-const MenuItemCard = ({ name, description, price }) => {
+const MenuItemCard = ({ name, description, price, imageId, showImage }) => {
   console.log(price);
   return (
     <>
@@ -17,6 +18,13 @@ const MenuItemCard = ({ name, description, price }) => {
         buttonLabel="I want it"
         onClick={() => alert("Clicked 🙂")}
         overrides={{ Root: { style: { border: "none" } } }}
+       
+        image={{
+          src: IMAGE_CDN_URL + imageId,
+          backgroundPosition: 'center',
+          layout: IMAGE_LAYOUT.trailing,
+          ariaLabel: 'A table with freshly prepared food',
+        }}
       />
       <StyledDivider $size={SIZE.cell} />
     </>
