2599177b3999637945850cf66d06644b7f0adf35
amarmanhala <amar.manhala@gmail.com>
2023-04-20 20:53:24 -0400

    Format code

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,34 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
-import { IMAGE_CDN_URL } from "../config";
 import useRestaurantMenu from "../../utils/useRestaurantMenu";
-import { StarRating } from "baseui/rating";
-import { HintDot, COLOR } from "baseui/badge";
 import {
   DisplayLarge,
   DisplayMedium,
   DisplaySmall,
   DisplayXSmall,
-} from 'baseui/typography';
-import {
-  ParagraphLarge,
-  ParagraphMedium,
-  ParagraphSmall,
-  ParagraphXSmall,
-  LabelLarge,
-  LabelMedium,
-  LabelSmall,
-  LabelXSmall,
-} from 'baseui/typography';
-import {Tag, VARIANT, SIZE} from 'baseui/tag';
+  HeadingMedium,
+  HeadingXSmall,
+} from "baseui/typography";
 import RestaurantHeader from "./RestaurantHeader";
 import Shimmer from "./Shimmer";
 import MenuItemCard from "./MenuItemCard";
 
 const RestaurantMenu = () => {
-
-
-
   // How to read dynamic params.
   const { id } = useParams();
 
@@ -36,78 +21,62 @@ const RestaurantMenu = () => {
 
   const arr = [];
 
-  test();
+  getRestaurantData();
 
-  function getMenuItems(obj) {
-    
-    if(obj?.card?.title !== undefined && obj?.card?.itemCards !== undefined) {
-      //console.log(obj.card);
-      return obj.card;
-
-    //return {title, itemCards} = obj.card;
-    
-    }
-   // if (obj.card)
-  }
-
-  
-async function test() {
-  try {
-    if(restaurantDetail.length !== 0) {
-      console.log(restaurantDetail?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
-      restaurantDetail?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.map(eachCard => {
-        if(getMenuItems(eachCard?.card) !== undefined) {
-          arr.push(getMenuItems(eachCard?.card))
-        }
-      });
+  function getMenuItems(listOfMenus) {
+    try {
+      if (
+        listOfMenus?.card?.title !== undefined &&
+        listOfMenus?.card?.itemCards !== undefined
+      ) {
+        return listOfMenus.card;
+      }
+    } catch (error) {
+      console.log("There is something wrong...");
     }
   }
-  catch(error) {
-    console.log(error);
+
+  async function getRestaurantData() {
+    try {
+      if (restaurantDetail.length !== 0) {
+        console.log(
+          restaurantDetail?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR
+            ?.cards
+        );
+        restaurantDetail?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.map(
+          (eachCard) => {
+            if (getMenuItems(eachCard?.card) !== undefined) {
+              arr.push(getMenuItems(eachCard?.card));
+            }
+          }
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
-  
-}
 
-
-
-
- 
-
- // console.log("This is new one", arr);
-
-  //console.log(restaurantDetail?.data?.cards[2].groupedCard?.cardGroupMap?.REGULAR?.cards);
-
-  return restaurantDetail.length === 0 ? (<Shimmer />) : (
+  return restaurantDetail.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="container-restaurant-menu">
-     <RestaurantHeader data={restaurantDetail?.data?.cards[0]?.card?.card?.info} />
-     
-     
-     
+      <RestaurantHeader
+        data={restaurantDetail?.data?.cards[0]?.card?.card?.info}
+      />
+
       <div>
-        <h1>Menu</h1>
-       
-        <ul>
-      
-
-
-{arr.map(each => {
-  
-  
-  
-  //console.log(each?.itemCards);
-  return <>
-  <h1>{each?.title}</h1>
-  { each?.itemCards.map(j => {
-    console.log(j.card.info);
-    return  <MenuItemCard {...j.card.info} />
-    
-  }) }
- 
-  </>
-  
-  
-})}
-        </ul>
+        {arr.map((each) => {
+          //console.log(each?.itemCards);
+          return (
+            <>
+              <HeadingMedium>{each?.title}</HeadingMedium>
+              {each?.itemCards.map((j) => {
+                console.log(j.card.info);
+                return <MenuItemCard {...j.card.info} />;
+              })}
+            </>
+          );
+        })}
       </div>
     </div>
   );
